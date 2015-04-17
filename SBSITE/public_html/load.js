3242ddc87d162ad28ac41deb7d6f8fab91db1912
akshaynathr <akshaynathr@gmail.com>
2015-04-17 20:17:07 +0530

    Added rss feed

--- SBSITE/public_html/load.js
+++ SBSITE/public_html/load.js
@@ -14,13 +14,48 @@ function load()
 
 $( document ).ready(load);
 
+$(document).ready(function(){$('#preloader').show(); $('#workspace').load('pages/page2.html #home');$('#preloader').fadeOut('slow');
+            $('#sidebar').toggleClass('open');
+
+$('#sidebar').animate({left:"0px"},1000);
+});
+
+//resizes on load
+$(document).ready(function(){ 
+    $('#workspace').height($(window).height()-40);
+
+  $('#rightbar').height($(window).height()-40);  
+});
+
+//resizes on window change
+$(window).resize(function(){ var w=$(window).height();
+    
+  $('#workspace').height($(window).height()-40);
+
+  $('#rightbar').height($(window).height()-40);  
+
+});
+
+
 $(window).load(function() {
 		// Animate loader off screen
 		$("#preloader").fadeOut("slow");;
 	});
 
 $(document).ready(function()
-{
+{ //smooth scrolling 
+    var page = $('#workspace');  // set to the main content of the page   
+    $(window).mousewheel(function(event, delta, deltaX, deltaY){
+        if (delta < 0) page.scrollTop(page.scrollTop() + 65);
+        else if (delta > 0) page.scrollTop(page.scrollTop() - 65);
+        return false;
+    });
+    
+    
+            //adjusting workspace size
+    var w=$(window).height();
+    
+  $('#workspace').height($(window).height()-40);
     $('#sidebar').on('click',function(event){
         
         event.preventDefault();
