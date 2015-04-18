324554289ddc0d245bdddfa8446fb4b9f4808927
akshaynathr <akshaynathr@gmail.com>
2015-04-18 10:28:57 +0530

    events timeline added. Need to edit it for proper use

--- SBSITE/public_html/load.js
+++ SBSITE/public_html/load.js
@@ -17,11 +17,11 @@ function load()
 
 $( document ).ready(load);
 
-$(document).ready(function(){$('#preloader').show(); $('#workspace').load('pages/page2.html #home');$('#preloader').fadeOut('slow');
-            $('#sidebar').toggleClass('open');
+//$(document).ready(function(){$('#preloader').show(); $('#workspace').load('pages/page2.html #home');$('#preloader').fadeOut('slow');
+  //          $('#sidebar').toggleClass('open');
 
-$('#sidebar').animate({left:"0px"},1000);
-});
+//$('#sidebar').animate({left:"0px"},1000);
+//});
 
 //resizes on load
 $(document).ready(function(){ 
@@ -77,6 +77,7 @@ $(document).ready(function()
         
         //main menu loaders
          $('#home_nav').on('click',function(){ $('#preloader').show(); $('#workspace').load('pages/page2.html #home');$('#preloader').fadeOut('slow');});
+        $('#events').on('click',function(){ $('#preloader').show(); $('#workspace').load('timeline.html');$('#preloader').fadeOut('slow');});
         
         
         
